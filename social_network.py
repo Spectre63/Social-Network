6e1103cff6efab609e94841c702ff7ae15d4bf3e
Alyaman Alnaby <spectre@Alyamans-MacBook-Air.local>
2025-02-25 10:02:15 +0000

    Adding more comints

--- social_network.py
+++ social_network.py
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-class Node:
-    def __init__(self, value=None):
-        self.value = value
-        self.connections = []
 
-class Graph:
-    ...
-=======
 class Members:# Abdullah, A members class (vertex)
     def __init__(self, name, age, interests):
         self.name = name
@@ -35,7 +27,7 @@ class Graph: #Abdullah, a graph class to represent the network
         else:
             print("member already exists")
 
-    def remove_member(self, member): #A method to remove members
+    def remove_member(self, member): # Aydin, method to remove members
         if member.name in self.members:
             del self.members[member.name]
             for other_member in self.members.values(): #loops through the other members
@@ -44,7 +36,7 @@ class Graph: #Abdullah, a graph class to represent the network
         else:
             print("Member not found")
     
-    def add_adjacent(self, from_member, to_member, weight = 0): # AL YAMAN, Add an edge between two members
+    def add_adjacent(self, from_member, to_member, weight = 0): # Al yaman, Add an edge between two members
         self.members[from_member.name].add_adjacent(to_member.name, weight)
         if not self.directed: # If undirected, add reverse edge
             self.members[to_member.name].add_adjacent(from_member.name, weight)
@@ -54,16 +46,16 @@ class Graph: #Abdullah, a graph class to represent the network
         if not self.directed: # If undirected, remove reverse edge
             self.members[to_member.name].remove_adjacent(from_member.name)
     
-    def get_member_friends(self, member): # Get all friends of a member
+    def get_member_friends(self, member): #Al yaman, Get all friends of a member
         return self.members[member.name].get_adjacent()
     
-    def find_mutual_friends(self, member1, member2): # Al yaman, Find mutual friends between two members
+    def find_mutual_friends(self, member1, member2): # Aydin, Find mutual friends between two members
         member1_friends = set(self.members[member1.name].get_adjacent())
         member2_friends = set(self.members[member2.name].get_adjacent())
         mutual_friends = member1_friends.intersection(member2_friends)
         return mutual_friends
 
-    def recommend_friends(self, member): # Al Yaman, Recommend friends based on mutual connections
+    def recommend_friends(self, member): # Aydin, Recommend friends based on mutual connections
         member_vertex = self.members[member.name]
         member_adjacent = set(member_vertex.get_adjacent())
         friend_recommendations = set() # Use a set to avoid duplicates
@@ -78,30 +70,31 @@ class Graph: #Abdullah, a graph class to represent the network
                 friend_recommendations.add(friend_of_friend) 
         return list(friend_recommendations)  # Return recommended friends
 
-#----usage example----
-# graph = Graph()
-# p1 = Members("Abdullah", "19", "Volleyball, Video games")
-# p2 = Members("Abdullahi", "18", "Mountain Bikes, Cars")
-# p3 = Members("Hassan", "18", "Football, Rubics Cube")
-# p4 = Members("Aydin", "123", "Football, Coding")
-#
-#
-# graph.add_member(p1)
-# graph.add_member(p2)
-# graph.add_member(p3)
-# graph.add_member(p4)
-#
-#
-# graph.add_adjacent(p1, p2, 5)
-# graph.add_adjacent(p1, p3, 3)
-# graph.add_adjacent(p2, p3, 7)
-# graph.add_adjacent(p3, p4, 9)
-#
-# print(graph.members.keys())
-# graph.remove_member(p1)
-# print(graph.members.keys())
-#
-# print(graph.get_member_friends(p1))
-# graph.remove_adjacent(p1, p2)
-# print(graph.get_member_friends(p1))
->>>>>>> b65941ac460b73e9929ee5e4a21ffc83b0af9676
+'''
+# ----usage example----
+graph = Graph()
+p1 = Members("Abdullah", "19", "Volleyball, Video games")
+p2 = Members("Abdullahi", "18", "Mountain Bikes, Cars")
+p3 = Members("Hassan", "18", "Football, Rubics Cube")
+p4 = Members("Aydin", "123", "Football, Coding")
+
+
+graph.add_member(p1)
+graph.add_member(p2)
+graph.add_member(p3)
+graph.add_member(p4)
+
+
+graph.add_adjacent(p1, p2, 5)
+graph.add_adjacent(p1, p3, 3)
+graph.add_adjacent(p2, p3, 7)
+graph.add_adjacent(p3, p4, 9)
+
+print(graph.get_member_friends(p1))
+graph.remove_adjacent(p1, p2)
+print(graph.get_member_friends(p1))
+
+print(graph.members.keys())
+graph.remove_member(p1)
+print(graph.members.keys())
+'''
